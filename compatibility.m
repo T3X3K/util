6d401a7c5a4cf1fe1ad316aa8bd11d330f29ba0e
T3X3K <armansingh.bains@studenti.unipd.it>
2022-03-21 17:16:13 +0100

    added compatibility

---
+++ compatibility.m
@@ -0,0 +1,20 @@
+function [lambdas] = compatibility(v1,v2,name)
+% function to calculate compatibility betwwen numbers
+% v1 must contain the values, while vw their uncertainty (standard deviation)
+% a result less than 1 means very good, less than 2 but more than 1 good,
+% less than 3 but more than 2 acceptable, more than 4 incompatible
+% the third input is the name in which the results are going to be printed
+% in latex format
+
+    lambdas = [];
+
+    for i = 1:(size(v1,1)-1)
+        for j = (i+1):size(v1,1)
+            abs(v1(i) - v(j)) / sqrt(v2(i)^2 + v2(j)^2)
+            file = fopen(name, "\lambda_{%i%i}: %f", j, i,)
+        end
+    end
+
+    
+    return
+end
