f4d64c9952503dd6cd154ff2d69e677abf4ea904
T3X3K <76218261+T3X3K@users.noreply.github.com>
2022-09-10 12:52:33 +0200

    Update media.m

--- media.m
+++ media.m
@@ -1,16 +1,13 @@
-function [media,errore]=media(v,var)
+function [media,errore]=media(v,err)
 %function to calculate weghted average.
 %the formula for this average is derived
 %from the least square methods
 %v is a vector that contains the values
-%var is a vector that contains the 
-%variances of the respective entries in v
-    num = 0;
-    den = 0;
-    for i = 1:size(v,1)
-        num = num + v(i,1)/var(i,1);
-        den = den + 1/var(i,1); 
-    end
+%err is a vector that contains the 
+%uncertainty of the respective entries in v
+
+    num = sum(v./err.^2);
+    den = sum(1./err.^2);
     media = num/den;
     errore = sqrt(1/den);
 
