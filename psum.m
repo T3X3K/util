3e5537d86141023dc9745a918084fc673a84442b
T3X3K <76218261+T3X3K@users.noreply.github.com>
2022-10-12 12:07:58 +0200

    Create psum.m
    funztion to calculate parallel sum

---
+++ psum.m
@@ -0,0 +1,10 @@
+funztion summ = psum(v)
+% insert a matrix and it will give 
+% you the reciprocal of the
+% sum of the reciprocal 
+% of all the elements
+% of each column
+
+  summ = 1./sum(1./v);
+return
+end
